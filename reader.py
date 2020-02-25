b8b78a66ea8a702461f920496ef306dd34ad5835
Josefin Enehage <josefin.enehage@gmail.com>
2020-02-25 22:44:27 +0100

    added functionality for using several urls

--- reader.py
+++ reader.py
@@ -1,26 +1,37 @@
+import argparse
 import feedparser
 import notification
+parser = argparse.ArgumentParser(description='Send in URLS you want notification from')
+parser.add_argument('urls', nargs='+')
+args = parser.parse_args()
 
-feed = feedparser.parse("https://sandrabeijer.elle.se/feed/")
+for url in args.urls:
+    file_name = url
+    if file_name.startswith("https://"):
+        file_name = file_name.replace("https://", "")
+    elif file_name.startswith("http://"):
+        file_name = file_name.replace("http://", "")
+    else:
+        print("not valid URL")
 
-entry = feed.entries[0]
+    feed = feedparser.parse(url)
+    try:
+        entry = feed.entries[0]
+    except IndexError:
+        print("URL is not working")
 
-try:
-    file = open("sandrabeijer.txt", "r+")
-    saved = file.read()
-    if saved != entry.published:
-        notification.send_notification("Sandra Beijer", entry.title, entry.link)
+    first_dot = file_name.find(".")
+    file_name = file_name[:first_dot]
 
-except FileNotFoundError:
-    file = open("sandrabeijer.txt", "w")
+    try:
+        file = open(f'{file_name}.txt', 'r+')
+        saved = file.read()
+        if saved != entry.published:
+            notification.send_notification(file_name, f"Nytt inlägg {entry.title}", entry.link)
+    except FileNotFoundError:
+        file = open(f'{file_name}.txt', 'w')
 
-file.seek(0)
-file.truncate()
-file.write(entry.published)
-file.close()
-
-"""
-kolla om nytt blogginlägg publicerats
-hämta datum från rss feed och jämföra med sparat datum. Om dessa inte matchar skicka notis. 
-När script är klart, uppdatera lokal fil med senaste inläggets datum
-"""
+    file.seek(0)
+    file.truncate()
+    file.write(entry.published)
+    file.close()
